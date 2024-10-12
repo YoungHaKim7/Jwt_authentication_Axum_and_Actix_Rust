e367fa9831fa266bbfdcee2c3c342f1bd02e5509
GYoung <ytok1108@aol.com>
2024-10-12 21:19:57 +0900

    hello world

--- 03_actix_htmx_training/a01_postmodern_rust_htmx/src/main.rs
+++ 03_actix_htmx_training/a01_postmodern_rust_htmx/src/main.rs
@@ -1,4 +1,22 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use lazy_static::lazy_static;
+use tera::Tera;
+
+// lazy_static! {
+//     pub static ref TEMPLATES: Tera = {
+//         let source = "templates/**/*";
+//         let mut tera = match Tera::new(source) {
+//             Ok(t) => t,
+//             Err(e) => {
+//                 eprintln!("Parsing error(s): {}", e);
+//                 ::std::process::exit(1);
+//             }
+//         };
+//         tera.autoescape_on(vec![".html", ".sql"]);
+//         tera.register_filter("do_nothing", do_nothing_filter);
+//         tera
+//     };
+// }
 
 #[get("/")]
 async fn index() -> impl Responder {
